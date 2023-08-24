5abbe93dd1f885b1089ca7b027ba4149d95c00d5
Сергей Дубовский <svdub@mail.ru>
2023-08-24 12:52:21 +0300

    откорректирована версия java

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM openjdk:8-jdk-alpine
+FROM oopenjdk:11-jdk-alpine
 
 EXPOSE 8080
 
---
+++ src/test/java/ru/netology/spring_boot_rest1/SpringBootRest1ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.netology.spring_boot_rest1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRest1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
